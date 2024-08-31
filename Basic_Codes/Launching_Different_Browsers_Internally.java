9a4c05b580b1e600e6bcdf035f5772945d49bfff
CHANDRA KUMAR M <144804619+Chan123-cloud@users.noreply.github.com>
2024-08-31 17:43:00 +0530

    Add files via upload

---
+++ Basic_Codes/Launching_Different_Browsers_Internally.java
@@ -0,0 +1,25 @@
+package Basic_Codes;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import org.openqa.selenium.edge.EdgeDriver;
+
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Launching_Different_Browsers_Internally {
+
+	public static void main(String[] args) {
+
+		ChromeDriver A = new ChromeDriver();  // Launching the Chrome_Browser
+		
+		EdgeDriver B = new EdgeDriver();      // Launching the Edge_Browser
+		
+		FirefoxDriver C = new FirefoxDriver();  // Launching the Fire_fox Browser
+		
+		
+		
+	//	Note : If we want to Launch the Browser that specific Browser should present in our machine.
+		
+	}
+
+}
